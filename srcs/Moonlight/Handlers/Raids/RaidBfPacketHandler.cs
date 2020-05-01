50873fc91b8508b5b61bedc0892ff2f98cfb6d48
František Boháček <fandabohacek@gmail.com>
2020-05-02 01:36:09 +0200

    Add raid packet handlers

---
+++ srcs/Moonlight/Handlers/Raids/RaidBfPacketHandler.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moonlight.Clients;
+using Moonlight.Event;
+using Moonlight.Event.Raids;
+using Moonlight.Game.Raids;
+using Moonlight.Packet.Raid;
+
+namespace Moonlight.Handlers.Raids
+{
+    internal class RaidBfPacketHandler : PacketHandler<RaidBfPacket>
+    {
+        private readonly IEventManager _eventManager;
+
+        public RaidBfPacketHandler(IEventManager eventManager)
+        {
+            _eventManager = eventManager;
+        }
+
+        protected override void Handle(Client client, RaidBfPacket packet)
+        {
+            Raid raid = client.Character.Raid;
+
+            if (raid == null || raid.Ended)
+            {
+                client.Character.Raid = new Raid
+                {
+                    Status = RaidStatus.Unknown
+                };
+            }
+
+            if (packet.Type == 0)
+            {
+                raid.Status = RaidStatus.InProgress;
+                raid.StartTime = DateTime.Now;
+            }
+            else if (packet.Type == 1)
+            {
+                raid.Status = RaidStatus.Successful;
+            }
+            else
+            {
+                raid.Status = RaidStatus.Fail;
+            }
+
+            _eventManager.Emit(new RaidStatusChangedEvent(client)
+            {
+                Raid = raid
+            });
+        }
+    }
+}
---
+++ srcs/Moonlight/Handlers/Raids/RaidMbfPacketHandler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moonlight.Clients;
+using Moonlight.Game.Raids;
+using Moonlight.Packet.Raid;
+
+namespace Moonlight.Handlers.Raids
+{
+    internal class RaidMbfPacketHandler : PacketHandler<RaidMbfPacket>
+    {
+        protected override void Handle(Client client, RaidMbfPacket packet)
+        {
+            Raid raid = client.Character.Raid;
+
+            raid.ButtonLockerCurrent = packet.ButtonLockerCurrent;
+            raid.ButtonLockerInitial = packet.ButtonLockerInitial;
+            raid.MonsterLockerCurrent = packet.MonsterLockerCurrent;
+            raid.MonsterLockerInitial = packet.MonsterLockerInitial;
+            raid.CurrentLifes = packet.CurrentLifes;
+            raid.InitialLifes = packet.InitialLifes;
+        }
+    }
+}

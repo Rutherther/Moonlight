ee4c69d00e81424a660a8a8a18a19cfbf479308a
František Boháček <fandabohacek@gmail.com>
2020-05-02 01:36:13 +0200

    Add raid status enum

---
+++ srcs/Moonlight/Game/Raids/RaidStatus.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonlight.Game.Raids
+{
+    public enum RaidStatus
+    {
+        Preparation,
+        InProgress,
+        Left,
+        Fail,
+        Successful,
+
+        Unknown
+    }
+}

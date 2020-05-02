4c2d7ab0f961e5e8b2138320fb807326bbae43be
František Boháček <fandabohacek@gmail.com>
2020-05-02 19:56:15 +0200

    Add IServiceConfiguration

---
+++ srcs/Moonlight/Core/IServiceConfiguration.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Moonlight.Core
+{
+    public interface IServiceConfiguration
+    {
+        void ConfigureServices(MoonlightAPI api, IServiceCollection services);
+    }
+}

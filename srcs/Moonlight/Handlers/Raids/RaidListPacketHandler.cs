1cb84af619dc5d3c2af6dc211718f248a5dd6bcb
František Boháček <fandabohacek@gmail.com>
2020-05-06 09:17:02 +0200

    Fire raid initialized event

--- srcs/Moonlight/Handlers/Raids/RaidListPacketHandler.cs
+++ srcs/Moonlight/Handlers/Raids/RaidListPacketHandler.cs
@@ -11,19 +11,31 @@ namespace Moonlight.Handlers.Raids
 {
     public class RaidListPacketHandler : PacketHandler<RaidListPacket>
     {
+        private readonly IEventManager _eventManager;
+
+        public RaidListPacketHandler(IEventManager eventManager)
+        {
+            _eventManager = eventManager;
+        }
+
         protected override void Handle(Client client, RaidListPacket packet)
         {
             Raid raid = client.Character.Raid;
 
             if (raid == null || raid.Ended)
             {
-                client.Character.Raid = new Raid
+                raid = client.Character.Raid = new Raid
                 {
                     Status = RaidStatus.Preparation,
                     RaidId = packet.RaidId,
                     MinimumLevel = packet.MinimumLevel,
                     MaximumLevel = packet.MaximumLevel
                 };
+
+                _eventManager.Emit(new RaidInitializedEvent(client)
+                {
+                    Raid = raid
+                });
             }
 
             // TODO: handle players

1b194ae2a76d34125df3006bc99ff19e99585c72
František Boháček <fandabohacek@gmail.com>
2020-05-09 09:25:19 +0200

    Add default clause to switch statement

--- srcs/Moonlight/Handlers/Raids/RaidPacketHandler.cs
+++ srcs/Moonlight/Handlers/Raids/RaidPacketHandler.cs
@@ -69,6 +69,8 @@ namespace Moonlight.Handlers.Raids
                         Raid = raid
                     });
                     break;
+                default:
+                    break;
             }
         }
     }

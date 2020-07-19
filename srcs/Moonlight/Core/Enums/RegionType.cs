32c1c3ad7aa0168ce0de06a957151e4d79f4a876
František Boháček <fandabohacek@gmail.com>
2020-07-19 12:16:57 +0200

    feat: add RegionTypes

---
+++ srcs/Moonlight/Core/Enums/RegionType.cs
@@ -0,0 +1,15 @@
+namespace Moonlight.Core.Enums
+{
+    public enum RegionType
+    {
+        EN,
+        DE,
+        FR,
+        IT,
+        PL,
+        ES,
+        CS,
+        TR,
+        RU,
+    }
+}

95cd639722cf14736831d3967aa4112ebfd8620b
František Boháček <fandabohacek@gmail.com>
2020-08-24 17:07:20 +0200

    fix: TcpClient GetState extension method for .NET core applications

--- srcs/Moonlight.Remote/Extensions/TcpClientExtensions.cs
+++ srcs/Moonlight.Remote/Extensions/TcpClientExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 
@@ -9,11 +10,23 @@ namespace Moonlight.Remote.Extensions
     {
         public static TcpState GetState(this TcpClient tcpClient)
         {
+            if (tcpClient.Client.LocalEndPoint == null)
+            {
+                return TcpState.Unknown;
+            }
+            
             try
             {
+                var endPoint = (IPEndPoint)tcpClient.Client.LocalEndPoint;
+                IPAddress ipv4 = endPoint.Address;
+                if (ipv4.IsIPv4MappedToIPv6)
+                {
+                    ipv4 = ipv4.MapToIPv4();
+                }
+                
                 TcpConnectionInformation foo = IPGlobalProperties.GetIPGlobalProperties()
                     .GetActiveTcpConnections()
-                    .SingleOrDefault(x => x.LocalEndPoint.Equals(tcpClient.Client.LocalEndPoint));
+                    .SingleOrDefault(x => x.LocalEndPoint.Address.Equals(ipv4) && x.LocalEndPoint.Port == endPoint.Port);
                 return foo != null ? foo.State : TcpState.Unknown;
             }
             catch (ObjectDisposedException e)
